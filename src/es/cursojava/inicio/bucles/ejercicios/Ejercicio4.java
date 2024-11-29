abea974fd1ec4012c391d34ae9a23b8ee28d9147
Tardes <Tardes@GET-04>
2024-11-29 16:57:35 +0100

    Ejercicio Bucles 3

---
+++ src/es/cursojava/inicio/bucles/ejercicios/Ejercicio4.java
@@ -0,0 +1,16 @@
+package es.cursojava.inicio.bucles.ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		//Mostrar todas las tablas de multiplicar hasta el número que solicitéis al usuario
+		
+		Scanner scan = new Scanner(System.in);
+		
+
+	}
+
+}
