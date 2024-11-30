db7db0a4ccf743a274d82c87e6b07cd6ece89752
HP <HP@DESKTOP-2KJCU7Q>
2024-11-30 14:02:10 +0100

    Ejercicio Bucles 8

--- src/es/cursojava/inicio/bucles/ejercicios/Ejercicio8.java
+++ src/es/cursojava/inicio/bucles/ejercicios/Ejercicio8.java
@@ -15,10 +15,18 @@ public class Ejercicio8 {
 		
 		System.out.println("¿Cuántos tenistas se apuntan al torneo?");
 		int numTenistas = scan.nextInt();
+		int top10 =0;
 		
-		for(int i=0 ; i<numTenistas ; i++) {
+		for(int i=1 ; i<numTenistas ; i++) {
+			System.out.println("Ingresa el puesto ATP del tenista " + i + ": ");
+			int puestoATP = scan.nextInt();
 			
+			if(puestoATP>=1 && puestoATP<=10) {
+				top10++;
+			}
 		}
+		
+		System.out.println("Número de tenistas en el top 10 :" + top10);
 
 	}
 
---
+++ src/es/cursojava/inicio/string/ejercicios/Ejercicio1.java
@@ -0,0 +1,10 @@
+package es.cursojava.inicio.string.ejercicios;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
