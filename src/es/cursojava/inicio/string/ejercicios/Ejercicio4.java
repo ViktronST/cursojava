856bb9e50237522e0de1e7793055f452905a9638
Tardes <Tardes@host.docker.internal>
2024-12-04 20:44:55 +0100

    Ejercicio 4 incompleto

---
+++ src/es/cursojava/inicio/string/ejercicios/Ejercicio4.java
@@ -0,0 +1,18 @@
+package es.cursojava.inicio.string.ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		//Escribe un programa que reciba un String con varias palabras separadas por espacios y determine cuál es la palabra más larga.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Introduce un texto:");
+		String texto = scan.nextLine();
+		
+		
+	}
+
+}
