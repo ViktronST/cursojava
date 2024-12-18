d1d39a3ff74bcbfa832b5676e8aff9a69729775e
VikiS <VikiS@DESKTOP-LMJUMHJ.home>
2024-12-18 22:51:46 +0100

    a

--- src/es/cursojava/inicio/arrays/ejercicios/Ejercicio8.java
+++ src/es/cursojava/inicio/arrays/ejercicios/Ejercicio8.java
@@ -3,9 +3,24 @@ package es.cursojava.inicio.arrays.ejercicios;
 import java.util.Scanner;
 
 public class Ejercicio8 {
-
     public static void main(String[] args) {
         
+//    	Preguntar cuantas aulas tiene un colegio, 
+//    	Suponiendo que en cada aula caben 5 alumnos
+//
+//    	1. Pedir datos
+//    		+ Por cada mesa vacía de un aula solicitar el nombre del alumno
+//    	2. Mostrar alumnos por aula
+//    		+ Mostrar por cada aula los nombres de los alumnos
+//    	3. Buscar alumno
+//    		+ Pedir nombre del alumno a buscar
+//    		+ Indicar en qué aula se encuentra
+//    	4. Borrar alumnos
+//    		+ Confirmar borrado
+//    	5. Salir
+//
+//    	Selecciona una opción
+    	
     	Scanner scan = new Scanner(System.in);
         System.out.println("¿Cuántas aulas tiene el colegio?");
         int numAulas = scan.nextInt();
