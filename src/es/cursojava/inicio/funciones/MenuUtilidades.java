c72cbdb11c3399fd644aec477f917aad7b262cc4
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-01-17 20:44:00 +0100

    MenuUtilidades

--- src/es/cursojava/inicio/funciones/MenuUtilidades.java
+++ src/es/cursojava/inicio/funciones/MenuUtilidades.java
@@ -1,29 +1,9 @@
 package es.cursojava.inicio.funciones;
 
-import java.util.Scanner;
-
 public class MenuUtilidades {
-public static void main(String[] args) {
-        
-//    	Preguntar cuantas aulas tiene un colegio, 
-//    	Suponiendo que en cada aula caben 5 alumnos
-//
-//    	1. Pedir datos
-//    		+ Por cada mesa vacía de un aula solicitar el nombre del alumno
-//    	2. Mostrar alumnos por aula
-//    		+ Mostrar por cada aula los nombres de los alumnos
-//    	3. Buscar alumno
-//    		+ Pedir nombre del alumno a buscar
-//    		+ Indicar en qué aula se encuentra
-//    	4. Borrar alumnos
-//    		+ Confirmar borrado
-//    	5. Salir
-//
-//    	Selecciona una opción
+    public static void main(String[] args) {
     	
-    	Scanner scan = new Scanner(System.in);
-        System.out.println("¿Cuántas aulas tiene el colegio?");
-        int numAulas = scan.nextInt();
+        int numAulas = Utilidades2.pideDatoNumerico("¿Cuántas aulas tiene el colegio?");
         String[][] aulas = new String[numAulas][5];
         int opcion;
         final int OPCION_SALIR = 5;
@@ -32,82 +12,18 @@ public static void main(String[] args) {
         	String[] opcionesMenu = {"\n = = = MENÚ = = =", "1. Introducir Datos de los Alumnos", "2. Mostrar Alumnos por Aula",
         			"3. Buscar Alumno", "4. Borrar Alumno", "5. Salir"};
             Utilidades2.pintarMenu(opcionesMenu);
-            opcion = scan.nextInt();
-            scan.nextLine();
+            opcion = Utilidades2.pideDatoNumerico("Introduce una opción: ");
 
             switch (opcion) {
-                case 1:
-                    for (int i = 0; i < numAulas; i++) {
-                        System.out.println("Aula " + (i + 1) + ":");
-                        for (int j = 0; j < 5; j++) {
-                            System.out.print("Introduce el nombre del alumno para la mesa " + (j + 1) + ": ");
-                            aulas[i][j] = scan.nextLine();
-                        }
-                    }
-                    break;
-
-                case 2:
-                    for (int i = 0; i < numAulas; i++) {
-                        System.out.println("Aula " + (i + 1) + ":");
-                        for (int j = 0; j < 5; j++) {
-                            System.out.println("Mesa " + (j + 1) + ": " + (aulas[i][j] == null ? "Vacío" : aulas[i][j]));
-                        }
-                    }
-                    break;
-
-                case 3:
-                    System.out.print("Introduce el nombre del alumno que deseas buscar: ");
-                    String nombreBuscado = scan.nextLine();
-                    boolean encontrado = false;
-
-                    for (int i = 0; i < numAulas; i++) {
-                        for (int j = 0; j < 5; j++) {
-                            if (aulas[i][j] != null && aulas[i][j].equalsIgnoreCase(nombreBuscado)) {
-                                System.out.println("El alumno " + nombreBuscado + " está en el aula " + (i + 1) + ", mesa " + (j + 1));
-                                encontrado = true;
-                                break;
-                            }
-                        }
-                        if (encontrado) {
-                        	break;
-                        }
-                    }
-
-                    if (!encontrado) {
-                        System.out.println("El alumno " + nombreBuscado + " no se encuentra en ninguna aula.");
-                    }
-                    break;
-
-                case 4:
-                    System.out.print("Introduce el nombre del alumno que deseas borrar: ");
-                    String nombreBorrar = scan.nextLine();
-                    boolean borrado = false;
-
-                    for (int i = 0; i < numAulas; i++) {
-                        for (int j = 0; j < 5; j++) {
-                            if (aulas[i][j] != null && aulas[i][j].equalsIgnoreCase(nombreBorrar)) {
-                                aulas[i][j] = null;
-                                System.out.println("El alumno " + nombreBorrar + " ha sido eliminado del aula " + (i + 1) + ", mesa " + (j + 1));
-                                borrado = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (!borrado) {
-                        System.out.println("El alumno " + nombreBorrar + " no se encuentra en ninguna aula.");
-                    }
-                    break;
-
-                case 5:
-                    System.out.println("\n- - - ¡Adiós! - - -");
-                    break;
-
-                default:
-                    System.out.println("Opción no válida. Por favor, elige una opción del menú.");
+                case 1: Utilidades2.datosAlumnos(aulas); break;
+                case 2: Utilidades2.mostrarAlumnos(aulas); break;
+                case 3: Utilidades2.buscarAlumno(aulas); break;
+                case 4: Utilidades2.borrarAlumno(aulas); break;
+                case 5: Utilidades2.pideDatoCadena("\n- - - ¡Adiós! - - -"); break;
+                default: Utilidades2.pideDatoCadena("\nOpción no válida. Por favor, elige una opción del menú.");
             }
         } while (opcion != OPCION_SALIR);
 
-        scan.close();
     }
+
 }
--- src/es/cursojava/inicio/funciones/Utilidades2.java
+++ src/es/cursojava/inicio/funciones/Utilidades2.java
@@ -4,26 +4,88 @@ import java.util.Scanner;
 
 public class Utilidades2 {
 	
-	public static int numAulas(int aulas) {
+	public static int pideDatoNumerico(String texto) {
+		System.out.print(texto);
+		Scanner scan = new Scanner(System.in);
+		int numero = scan.nextInt();
 		
+		return numero;
+	}
+	
+	public static String pideDatoCadena(String texto) {
+		System.out.print(texto);
+		Scanner scan = new Scanner(System.in);
+		String cadena = scan.nextLine();
+		
+		return cadena;
 	}
 	
 	public static void pintarMenu(String[] menuArray) {
 		for (String opcion : menuArray) {
 			System.out.println(opcion);
 		}
-		System.out.print("\nIntroduce una opción: ");
 	}
 	
-	public static int caso1(int[] numAulas) {
-    	Scanner scan = new Scanner(System.in);
-		for (int i = 0; i < numAulas; i++) {
+	public static void datosAlumnos (String[][] aulas){
+        for (int i = 0; i < aulas.length; i++) {
             System.out.println("Aula " + (i + 1) + ":");
             for (int j = 0; j < 5; j++) {
                 System.out.print("Introduce el nombre del alumno para la mesa " + (j + 1) + ": ");
-                aulas[i][j] = scan.nextLine();
-                return aulas[i][j];
+                aulas[i][j] = pideDatoCadena("");
             }
+        }
+    }
+
+	public static void mostrarAlumnos (String[][] aulas){
+		for (int i = 0; i < aulas.length; i++) {
+			System.out.println("Aula " + (i + 1) + ":");
+			for (int j = 0; j < 5; j++) {
+				System.out.println("Mesa " + (j + 1) + ": " + (aulas[i][j] == null ? "Vacío" : aulas[i][j]));
+			}
+		}
+	}
+
+	public static void buscarAlumno (String[][] aulas){
+		pideDatoCadena("Introduce el nombre del alumno que deseas buscar: ");
+        String nombreBuscado = pideDatoCadena("");
+        boolean encontrado = false;
+
+        for (int i = 0; i < aulas.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (aulas[i][j] != null && aulas[i][j].equalsIgnoreCase(nombreBuscado)) {
+                    System.out.println("El alumno " + nombreBuscado + " está en el aula " + (i + 1) + ", mesa " + (j + 1));
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (encontrado) {
+                break;
+            }
+        }
+
+        if (!encontrado) {
+        System.out.println("El alumno " + nombreBuscado + " no se encuentra en ninguna aula.");
+        }
+	}
+
+	public static void borrarAlumno (String[][] aulas){
+		pideDatoCadena("Introduce el nombre del alumno que deseas borrar: ");
+        String nombreBorrar = pideDatoCadena("");
+        boolean borrado = false;
+
+        for (int i = 0; i < aulas.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (aulas[i][j] != null && aulas[i][j].equalsIgnoreCase(nombreBorrar)) {
+                    aulas[i][j] = null;
+                    System.out.println("El alumno " + nombreBorrar + " ha sido eliminado del aula " + (i + 1) + ", mesa " + (j + 1));
+                    borrado = true;
+                    break;
+                }
+            }
+        }
+
+        if (!borrado) {
+            System.out.println("El alumno " + nombreBorrar + " no se encuentra en ninguna aula.");
         }
 	}
 }
