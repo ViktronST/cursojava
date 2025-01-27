4ad4607e63179131ba15d3f4d1a7ea8160bf6810
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-01-27 20:42:37 +0100

    EjerCine

--- src/es/cursojava/cinefilos/PruebaCine.java
+++ src/es/cursojava/cinefilos/PruebaCine.java
@@ -2,7 +2,15 @@ package es.cursojava.cinefilos;
 
 public class PruebaCine {
     public static void main(String[] args) {
-        Sala sala1 = new Sala(1, "El rey león", "[1][5]");
+        Cine cine = new Cine("Trinity", 1);
+        
+        Sala sala1 = new Sala(1, "El rey león", "30");
+        Sala sala2 = new Sala(2, "Tennet", "30");
+        Sala sala3 = new Sala(3, "Los Odiosos 8", "30");
+        
+        Espectador espectador = new Espectador("Juan", 20405060L);
+        Espectador espectador = new Espectador("María", 10509070H);
+        Espectador espectador = new Espectador("Ivan", 85274163Z);
         
     }
 }
