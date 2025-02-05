b48e61a67e0312e982035117f950ab1dc8f4e2d2
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-02-05 19:43:54 +0100

    Librerias y ejemplos

---
+++ src/es/cursojava/POO/herencia/concesionario/PruebaVehiculoProfe.java
@@ -0,0 +1,17 @@
+package es.cursojava.POO.herencia.concesionario;
+
+import es.cursojava.poo.herencia.ejercicios.vehiculos.MainVehiculos;    //Librería 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class PruebaVehiculoProfe {
+    public static void main(String[] args) {
+        MainVehiculos.main(args);
+    }
+}
