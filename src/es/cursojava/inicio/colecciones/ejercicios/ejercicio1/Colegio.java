476342f8bfc407ac7cf862e30fb6a8633b1f60f8
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-02-19 13:21:19 +0100

    MainAlumnos2.4

--- src/es/cursojava/inicio/colecciones/ejercicios/ejercicio1/Colegio.java
+++ src/es/cursojava/inicio/colecciones/ejercicios/ejercicio1/Colegio.java
@@ -1,5 +1,12 @@
 package es.cursojava.inicio.colecciones.ejercicios.ejercicio1;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import es.cursojava.POO.centroeducativo.Alumno;
+
 //import 
 
 // Lista de Colegios, con Mapa de Aulas y Lista de Alumnos...
@@ -7,8 +14,29 @@ package es.cursojava.inicio.colecciones.ejercicios.ejercicio1;
 public class Colegio {
     private String nombre;
     private String direccion;
-    //private Map<String, List<Alumno>> aulas;
+    private Map<String, List<Alumno>> aulas;
 
+    public Colegio(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.aulas = new HashMap<>();
+    }
 
+    public void agregarAula(String aula, List<Alumno> alumnos) {
+        aulas.put(aula, alumnos);
+    }
+
+    public void mostrarDatos() {
+        System.out.println("\n================ Distribución por Colegio ================");
+        System.out.println("Colegio: " + nombre + " - Dirección: " + direccion);
+        Set<String> aulas1 = aulas.keySet();
+        for (String aula : aulas1) {
+            System.out.println("Aula: " + aula);
+            List<Alumno> alumnos = aulas.get(aula);
+            for ( Alumno alumno : alumnos) {
+                System.out.println("\t" + alumno.getNombre() + " " + alumno.getApellido());
+            }
+        }
+    }
 
 }
