464ae7624e5442b7a247c96a08312fccde82519a
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-02-25 16:15:05 +0100

    Ejercicio1 - 1.0

--- src/es/cursojava/inicio/excepciones/ejercicios/ejercicio1/MainExcepcion.java
+++ src/es/cursojava/inicio/excepciones/ejercicios/ejercicio1/MainExcepcion.java
@@ -1,35 +1,19 @@
 package es.cursojava.inicio.excepciones.ejercicios.ejercicio1;
 
 import es.cursojava.POO.centroeducativo.Alumno;
-import es.cursojava.inicio.funciones.Utilidades;
 
 public class MainExcepcion {
     public static void main(String[] args) {
         MainExcepcion main = new MainExcepcion();
-        main.pideDatos();
-    }
-
-    private void pideDatos() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Introduce el nombre del alumno: ");
-            String nombre = Utilidades.pideDatoCadena("Federico");
-            System.out.println("Introduce el apellido del alumno: ");
-            String apellido = Utilidades.pideDatoCadena("García Lorca");
-            System.out.println("Introduce la edad del alumno: ");
-            int edad = Utilidades.pideDatoNumerico("38");
-            System.out.println("Introduce la nota media del alumno: ");
-            double notaMedia = Utilidades.pideDatoNumerico("8.5");
-            System.out.println("Introduce el email del alumno: ");
-            String email = Utilidades.pideDatoCadena("Lorca@gmail.com");
-        }
-        
+        main.creaAlumno("Viktor", "Stoyanov", 24, 10, "null@null.es");
     }
 
     private void creaAlumno(String nombre, String apellido, int edad, double notaMedia, String email) {
         try {
             Alumno alumno = new Alumno(nombre, apellido, edad, notaMedia, email);
-        } catch (NotaInvalidaExcepcion e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println(alumno);
+        } catch (NotaInvalidaExcepcion error) {
+            System.out.println("Error: " + error.getMessage());
         }
     }
 }
